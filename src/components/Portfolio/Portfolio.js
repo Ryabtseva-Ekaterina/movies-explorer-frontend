37ad62647b581b96613d3c2802217f853666777a
Ryabtseva-Ekaterina <ekaterina-ryabtseva.evp@yandex.ru>
2022-09-29 08:56:40 +0300

    добавить ховер ссылкам на главной

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,28 +7,28 @@ function Portfolio() {
         <section className='portfolio'>
             <h3 className='portfolio__title'>Портфолио</h3>
             <nav>
-                <ul className='porfolio__links'>
+                <ul className='portfolio__links'>
                     <li className = 'portfolio__link'>
-                        <a href='https://ryabtseva-ekaterina.github.io/how-to-learn/' target='blank' className='porfolio__nav-link'>
+                        <a href='https://ryabtseva-ekaterina.github.io/how-to-learn/' target='blank' className='portfolio__nav-link'>
                             <p className='portfolio__link-text'>Статичный сайт</p> 
                             <img className='portfolio__link-image' src={arrow} alt='стрелка'/>
                          </a>   
                     </li>
                     
 
-                    <hr className="potrfolio__line"/>
+                    <hr className="portfolio__line"/>
 
                     <li className = 'portfolio__link'>
-                        <a href='https://ryabtseva-ekaterina.github.io/russian-travel/index.html' target='blank' className='porfolio__nav-link'>
+                        <a href='https://ryabtseva-ekaterina.github.io/russian-travel/index.html' target='blank' className='portfolio__nav-link'>
                             <p className='portfolio__link-text'>Адаптивный сайт</p> 
                             <img className='portfolio__link-image' src={arrow} alt='стрелка'/>
                         </a>
                     </li>
 
-                    <hr className="potrfolio__line"/>
+                    <hr className="portfolio__line"/>
 
                     <li className = 'portfolio__link'>
-                        <a href='http://mesto.ryabtseva.nomoredomains.sbs' target='blank' className='porfolio__nav-link'>
+                        <a href='http://mesto.ryabtseva.nomoredomains.sbs' target='blank' className='portfolio__nav-link'>
                             <p className='portfolio__link-text'>Одностраничное приложение</p>
                             <img className='portfolio__link-image' src={arrow} alt='стрелка'/>
                         </a>

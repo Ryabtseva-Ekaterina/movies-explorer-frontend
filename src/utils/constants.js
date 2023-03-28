5c3ab45cad33522a47ed67bedfe11509f0bce438
Ryabtseva-Ekaterina <ekaterina-ryabtseva.evp@yandex.ru>
2023-03-28 10:04:04 +0300

    заменить урл бэка

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 const ShortTime = 40;
-const Base_Url = 'https://api.movies-exp.ryabtseva.nomoredomains.sbs';
+const Base_Url = 'https://movies-explorer-api-henna.vercel.app/';
 const CountOfMoviesPC = 3;
 const CountOfMoviesPhone = 2;
 

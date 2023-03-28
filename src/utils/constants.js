1c4157e891d061e9c19cd42d6d6334dff6bb4b85
Ryabtseva-Ekaterina <ekaterina-ryabtseva.evp@yandex.ru>
2023-03-28 10:06:11 +0300

    убрать слэш

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 const ShortTime = 40;
-const Base_Url = 'https://movies-explorer-api-henna.vercel.app/';
+const Base_Url = 'https://movies-explorer-api-henna.vercel.app';
 const CountOfMoviesPC = 3;
 const CountOfMoviesPhone = 2;
 

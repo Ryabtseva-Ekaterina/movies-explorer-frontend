acfb66c34bf0cddf7d0574eafbad6f9c02721052
Ryabtseva-Ekaterina <ekaterina-ryabtseva.evp@yandex.ru>
2023-03-31 14:10:57 +0300

    D:/Git/

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 const ShortTime = 40;
-const Base_Url = 'https://movies-explorer-api-henna.vercel.app';
+//const Base_Url = 'https://movies-explorer-api-henna.vercel.app';
+const Base_Url = 'http://localhost:3001';
 const CountOfMoviesPC = 3;
 const CountOfMoviesPhone = 2;
 
